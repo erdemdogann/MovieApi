13390527e9388ac57519af796a53b8b5047fe124
Erdem Doğan <107808469+erdemdogann@users.noreply.github.com>
2024-04-22 15:15:50 +0300

    Series Paging

---
+++ app/src/main/java/com/example/movieapi/data/paging/allmovie/AllMoviePagingAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.movieapi.data.paging.allmovie
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieapi.data.dto.movie.Result
+import com.example.movieapi.databinding.MovieCardBinding
+import com.example.movieapi.loadImage
+
+class AllMoviePagingAdapter :
+    PagingDataAdapter<Result, AllMoviePagingAdapter.Holder>(diffCallback) {
+
+    companion object {
+        val diffCallback = object : DiffUtil.ItemCallback<Result>() {
+            override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    class Holder(private val binding: MovieCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Result?) {
+            binding.apply {
+                movieName.text = movie?.title
+                movieImage.loadImage(movie?.poster_path)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(MovieCardBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+}
